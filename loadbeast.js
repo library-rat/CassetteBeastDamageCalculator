6fc405faf03df2999b54392eca174f167fe98e46
library-rat <1ucas_z@protonmail.com>
2025-08-01 14:03:19 +0200

    factorizing before adding character selection

--- loadbeast.js
+++ loadbeast.js
@@ -4,17 +4,27 @@ fetch('Beast.json')
      .then((response) => response.json())
      .then((json) => beastArray = json);
 
+const resultsBoxCharacter1 = document.querySelector(".result-box-character1");
+const inputBoxCharacter1 = document.getElementById("input-box-character1");
+const characterStat1 = document.getElementById("character1-stat");
 const resultsBoxBeast1= document.querySelector(".result-box-beast1");
 const inputBoxBeast1 = document.getElementById("input-box-beast1");
-const beastStat1 = document.getElementById("beast1-stat")
+const beastStat1 = document.getElementById("beast1-stat");
+const resultsBoxCharacter2 = document.querySelector(".result-box-character2");
+const inputBoxCharacter2 = document.getElementById("input-box-character2");
+const characterStat2 = document.getElementById("character2-stat");
 const resultsBoxBeast2= document.querySelector(".result-box-beast2");
 const inputBoxBeast2 = document.getElementById("input-box-beast2");
 const beastStat2 = document.getElementById("beast2-stat")
 
 
-inputBoxBeast1.onkeyup = function(){
+inputBoxBeast1.addEventListener("keyup", () => onInputText(inputBoxBeast1,resultsBoxBeast1));
+
+inputBoxBeast2.addEventListener("keyup", () => onInputText(inputBoxBeast2,resultsBoxBeast2));
+
+function onInputText(inputBox, resultsBox, isCharacter){
     let result = [];
-    let input = inputBoxBeast1.value;
+    let input = inputBox.value;
     console.log(input.length);
     if(input.length){
         console.log(beastArray);
@@ -25,21 +35,9 @@ inputBoxBeast1.onkeyup = function(){
         })
         //console.log(result); 
     }
-    display(result,resultsBoxBeast1);
+    display(result,resultsBox);
 }
 
-inputBoxBeast2.onkeyup = function(){
-    let result = [];
-    let input = inputBoxBeast2.value;
-    if(input.length){
-        console.log(beastArray);
-
-        result = beastArray.filter(function(element){
-            return element.name.toLowerCase().includes(input);
-        })
-    }
-    display(result,resultsBoxBeast2);
-}
 
 function display(result, resultsBoxUI){
     let beastnum = (resultsBoxUI == resultsBoxBeast1) ? 1 : (resultsBoxUI == resultsBoxBeast2) ? 2:  -1 ;
