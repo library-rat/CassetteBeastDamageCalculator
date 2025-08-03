e7f8878fd43c03bdf88771c24ca7016cf3dc12b2
library-rat <1ucas_z@protonmail.com>
2025-08-03 15:31:13 +0200

    adding player completion

--- loadbeast.js
+++ loadbeast.js
@@ -1,8 +1,19 @@
+let characterArray;
 let beastArray;
+let character1;
 let beast1;
+let character2;
+let beast2;
+
+
+
 fetch('Beast.json')
      .then((response) => response.json())
      .then((json) => beastArray = json);
+
+fetch('Character.json')
+     .then((response) => response.json())
+     .then((json) => characterArray = json)
 
 const resultsBoxCharacter1 = document.querySelector(".result-box-character1");
 const inputBoxCharacter1 = document.getElementById("input-box-character1");
@@ -17,10 +28,13 @@ const resultsBoxBeast2= document.querySelector(".result-box-beast2");
 const inputBoxBeast2 = document.getElementById("input-box-beast2");
 const beastStat2 = document.getElementById("beast2-stat")
 
+inputBoxCharacter1.addEventListener("keyup", () => onInputText(inputBoxCharacter1,resultsBoxCharacter1, true));
 
-inputBoxBeast1.addEventListener("keyup", () => onInputText(inputBoxBeast1,resultsBoxBeast1));
+inputBoxBeast1.addEventListener("keyup", () => onInputText(inputBoxBeast1,resultsBoxBeast1,false));
 
-inputBoxBeast2.addEventListener("keyup", () => onInputText(inputBoxBeast2,resultsBoxBeast2));
+inputBoxCharacter2.addEventListener("keyup", ()=> onInputText(inputBoxCharacter2,resultsBoxCharacter2,true));
+
+inputBoxBeast2.addEventListener("keyup", () => onInputText(inputBoxBeast2,resultsBoxBeast2,false));
 
 function onInputText(inputBox, resultsBox, isCharacter){
     let result = [];
@@ -28,44 +42,67 @@ function onInputText(inputBox, resultsBox, isCharacter){
     console.log(input.length);
     if(input.length){
         console.log(beastArray);
-        result = beastArray.filter(function(element){
+        if(isCharacter){
+            result = characterArray.filter(function(element){
+            return element.name.toLowerCase().includes(input);
+            })
+        }else{
+            result = beastArray.filter(function(element){
             console.log(element.name);
             console.log(element.name.toLowerCase().includes(input));
             return element.name.toLowerCase().includes(input);
         })
+        }
         //console.log(result); 
     }
-    display(result,resultsBox);
+    display(result,resultsBox,isCharacter);
 }
 
 
-function display(result, resultsBoxUI){
-    let beastnum = (resultsBoxUI == resultsBoxBeast1) ? 1 : (resultsBoxUI == resultsBoxBeast2) ? 2:  -1 ;
+function display(result, resultsBoxUI, isCharacter){
+    let num;
+    if(!isCharacter){
+        num = (resultsBoxUI == resultsBoxBeast1) ? 1 : (resultsBoxUI == resultsBoxBeast2) ? 2:  -1 ;
+    }else{
+        num = (resultsBoxUI == resultsBoxCharacter1) ? 1: (resultsBoxUI == resultsBoxCharacter2) ? 2: -1;
+    }
     resultsBoxUI.innerHTML = '';
     result.forEach((element) => {
         const li = document.createElement("li");
         li.textContent = element.name;
-        li.addEventListener("click", () => selectInput(element, beastnum));
+        li.addEventListener("click", () => selectInput(element, num, isCharacter));
         resultsBoxUI.appendChild(li)
     })
-
 }
 
-function selectInput(beast, beastnum){
+function selectInput(monster, num, isCharacter){
     
-    if(beastnum == 1){
-        beast1 = beast;
-        inputBoxBeast1.value = beast.name;
-        resultsBoxBeast1.innerHTML = '';
-        updateMonsterStat(beast1, beastStat1);
-
-    }else if(beastnum == 2){
-        beast2 = beast;
-        inputBoxBeast2.value = beast.name;
-        resultsBoxBeast2.innerHTML = '';
-        updateMonsterStat(beast2, beastStat2);
+    if (isCharacter){
+        if(num == 1){
+            character1 = monster;
+            inputBoxCharacter1.value = monster.name;
+            resultsBoxCharacter1.innerHTML = '';
+            updateMonsterStat(character1,characterStat1);
+        }else if(num == 2){
+            character2 = monster;
+            inputBoxCharacter2.value = monster.name;
+            resultsBoxCharacter2.innerHTML='';
+            updateMonsterStat(character2, characterStat2);
+        }
+    }else{
+        if(num == 1){
+            beast1 = monster;
+            inputBoxBeast1.value = monster.name;
+            resultsBoxBeast1.innerHTML = '';
+            updateMonsterStat(beast1, beastStat1);
+        }else if(num == 2){
+            beast2 = monster;
+            inputBoxBeast2.value = monster.name;
+            resultsBoxBeast2.innerHTML = '';
+            updateMonsterStat(beast2, beastStat2);
 
     }
+}
 }
 
 function fetchMonster(name){
@@ -86,7 +123,5 @@ function updateMonsterStat(monster, displayUI){
     "<input type=\"number\" value=\"" + rAtk+ "\">" +
     "<input type=\"number\" value=\"" + rDef+ "\">" +
     "<input type=\"number\" value=\"" + speed+ "\">";
-    displayUI.innerHTML = content;
-    
-    
+    displayUI.innerHTML = content;   
 }
